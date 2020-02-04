bb12df407fb79f73f351ebab1954fabcdeae06d2
saxena.shubham.2110@gmail.com <saxena.shubham.2110@gmail.com>
2020-02-05 01:59:27 +0530

    Added Multi bot support. Added Full rest bases support for xmpp.

--- sleekMulti.py
+++ sleekMulti.py
@@ -1,4 +1,4 @@
-import csv
+import csv, sys
 import base64
 from sleekxmpp import ClientXMPP
 from sleekxmpp.exceptions import IqError, IqTimeout
@@ -17,12 +17,16 @@ from browser3 import browser
 
 # Add sessions dict['authkey']=echobotobject
 
+#boturl='http://10.52.150.150:5007/sendMessage'
+port='5007'
 
 	
 @app.route("/sendMessage", methods = ['GET', 'POST', 'DELETE'])
-#format: Post Method {'uid' : 'userid', 'message' : 'message here', 'timestamp:' 'timestamp here'}
+#format: Post Method {'client': 'clientid' , 'pass' : 'passwd'  'uid' : 'userid', 'message' : 'message here', 'timestamp:' 'timestamp here'}
 def sendMessage():
 	message=request.get_json()
+	clientID=message['client']
+	clientPass=message['pass']
 	user=message['uid']
 	body=message['message']
 	timestamp=str(time.time())
@@ -32,7 +36,7 @@ def sendMessage():
 	return "Hello, World!"
 
 async def send_finally(clientID, clientPass, user, body):
-	echobot=app.echobot
+	echobot=app.echobot[clientID]
 	await echobot.asend_message(user,body)
 	
 
@@ -78,6 +82,8 @@ class EchoBot(ClientXMPP):
 		#print(msg,msg['id'])	
 		message1=str(msg['body'])
 		body['message']=message1
+		body['pass'] = self.passw
+		body['client'] = self.client
 		body['timestamp']=str(time.time())
 		a=browser()
 		payload=a.dicttoJson(body)
@@ -99,27 +105,45 @@ class EchoBot(ClientXMPP):
 		#print()
 		t1.start()
 	async def asend_message(self, user, body):
+		print(user, body)
 		self.send_message(mto=user, mbody=body, mtype='chat')
 		#seld.connect()
 		#self.process()
 	#def send_message(self, url, uid, message):
 		
 
-class Sessions(clientID, clientPass):
-	pass
+#class Sessions(clientID, clientPass):
+#	pass
 	# to be added session for all employees
 		
 
 
 if __name__ == "__main__": 
-	#for each in member of multidb.csv, create a new echobot with threading. 
-	echobot=EchoBot(clientID, clientPass)
-	echobot.boturl=boturl
-	echobot.serverhost=serverhost
-	echobot.connect_process()
-	app.echobot=echobot
-	app.run(host='0.0.0.0', port='5001', debug=True, use_reloader=False )
-	t1=threading.Thread(target=echobot.connect_process)
-	t1.start()
+	#for each in member of multidb.csv, create a new echobot with threading.
+	f=open('multidb.csv','r')
+	c=csv.reader(f)
+	app.echobot={} # Dictionary for all bots
+	for eachrow in c:
+		clientid=eachrow[0]
+		serverhost=eachrow[0].split('@')[1]
+		clientpass=eachrow[1]
+		boturl=eachrow[2]
+		echobot=EchoBot(clientid, clientpass)
+		echobot.boturl=boturl
+		echobot.client=clientid
+		echobot.passw=clientpass
+		echobot.connect_process()
+		app.echobot[clientid]=echobot
+		
+	#echobot=EchoBot(clientID, clientPass)
+	#echobot.boturl=boturl
+	#echobot.serverhost=serverhost
+	#echobot.connect_process()
+	#app.echobot=echobot
+	app.run(host='0.0.0.0', port=port, debug=True, use_reloader=False )
+	for each in app.echobot.keys():
+		echobot=app.echobot[each]
+		t1=threading.Thread(target=echobot.connect_process)
+		t1.start()
 
 
